1f1706cc1248ef9019bf68ef0ae1041c24594212
Marco Arteaga <marcoarteaga26@gmail.com>
2019-12-20 16:23:25 -0600

    Update to notification file

--- main_app/notifications.py
+++ main_app/notifications.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACf3e003bdb5344e1d9915b07ab6902841'
-auth_token = '6c8bd6658904a7dbc1bf35edeac611e5'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='Congrats you can code!!!!!!',
-                              from_='+12018244246',
-                              to='+12105288184'
+                              from_='+',
+                              to='+'
                           )
 
 print(message.sid)
