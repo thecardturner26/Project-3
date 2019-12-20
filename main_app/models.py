ac29ffe54db4e7f085dfbd1aebedbc0069c9ff93
Zachary Fero <zfero277@ymail.com>
2019-12-20 14:47:48 -0600

    Django database models created

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Patient:
-  def __init__(self, name, d_o_b, user_relation, healthcare, appointments, pills):
-    self.name = name
-    self.d_o_b = d_o_b
-    self.user_relation = user_relation
-    self.healthcare = healthcare
-    self.appointments = appointments
-    self.pills = pills
+class Patient(models.Model):
+  name = models.CharField(max_length=60)
+  d_o_b = models.DateField()
+  user_relation = models.CharField(max_length=30)
+  healthcare = models.CharField(max_length=50)
+  pills = models.ManyToManyField(Pills)
+  appointments = models.ManyToManyField(Appointments)
+  
+  def __str__(self):
+      return self.name
+  
+
     
 patients = [
     Patient('Zach', 8132001, 'Back-Man', 'Arkids', [{'name': 'Witch Doctor', 'date': '2019-12-19 17:17:24.943901', 'practioner': 'Nazeebo', 'location': 'Congress Ave, Austin, TX 78704', 'note' : ''}] ,[{'name': 'Joy', 'total': 14, 'days': {'monday': ['0800','0800'], 'tuesday':[''],'wednesday': ['0800'],'thursday': [''],'friday': [''],'saturday': [''],'sunday': ['']}, 'dosage': 3}] ),
@@ -16,17 +23,64 @@ patients = [
     Patient('Juan', 1011970, 'Front-Man', 'MarcoCare', [] , [])
 ]
 
-class Pills:
-    def __init__(self, name, total, days, times, dosage):
-        self.name = name
-        self.total = total
-        self.days = days
-        self.dosage = dosage
+class Pills(models.Model):
+    name = models.CharField(max_length=80)
+    total = models.IntegerField()
+    days = models.ManyToManyField(Days)
+    dosage = models.IntegerField()
 
+    def __init__(self, name, total, days, times, dosage):
+        
+        
+class Days(models.Model):
+     monday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     tuesday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     wednesday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     thursday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     friday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     saturday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     sunday = models.ArrayField(
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none),
+         models.TimeField(default=none)
+     )
+     def __str__(self):
+        return self
+        
 class Appointment:
-    def __init__(self, name, date, practioner, location, note):
-        self.name = name
-        self.date = date
-        self.practioner = practioner
-        self.location = location
-        self.note = note
+    name = models.CharField()
+    date = models.DateField()
+    practioner = models.CharField()
+    location = models.CharField()
+    note = models.TextField()
