d0d8c12d4a1b134a2767022d7c3a5bd492ea8b96
Zachary Fero <zfero277@ymail.com>
2020-01-01 13:10:46 -0600

    Creating Django Auth and the Appending the Patient model to the Users

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from datetime import date, datetime
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -86,10 +87,11 @@ class Patient(models.Model):
   healthcare = models.CharField(max_length=50)
   pills = models.ManyToManyField(Pills)
   appointments = models.ManyToManyField(Appointments)
-  
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   def __str__(self):
     return self.name
   
-        
-        
-        
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    patients = models.ManyToManyField(Patient)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
