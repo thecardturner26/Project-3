5651499d4949a97bbc80b0270b130ecd9eeabbdf
Marco Arteaga <marcoarteaga26@gmail.com>
2020-01-02 15:41:38 -0600

    create, delete, and update functianality middleware

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,10 +3,25 @@ from . import views
 
 
 urlpatterns = [
-    #
+    # Basic Urls
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login, name='login_user'),
     path('patients/', views.patients_index, name='index'),
     path('patients/', views.patients_details, name='details'),
     path('registration/', views.registration_signup, name='signup'),
+
+    # create,update, and delete paths for pills model
+    path('pills/create/', views.PillsCreate.as_view(), name='pills_create'),
+    path('pills/<int:pk>/update/', views.PillsUpdate.as_view(), name='pills_update'),
+    path('pills/<int:pk>/delete/', views.PillsDelete.as_view(), name='pills_delete'),
+
+    # create,update, and delete paths for appointments
+    path('appointments/create/', views.AppointmentsCreate.as_view(), name='appointments_create'),
+    path('appointments/<int:pk>/update/', views.AppointmentsUpdate.as_view(), name='appointments_update'),
+    path('appointments/<int:pk>/delete/', views.AppointmentsDelete.as_view(), name='appointments_delete'),
+
+    # create,update, and delete paths for Patients
+    path('patients/create/', views.PatientsCreate.as_view(), name='patients_create'),
+    path('patients/<int:pk>/update/', views.PatientsUpdate.as_view(), name='patients_update'),
+    path('patients/<int:pk>/delete/', views.PatientsDelete.as_view(), name='patients_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Pills, Appointments, Patient
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -8,7 +10,8 @@ from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
-def login(request):
+# Updated from login to login_user
+def login_user(request):
     return render(request, 'login.html')
 def patients_details(request):
     return render(request, 'patients/details.html')
@@ -16,3 +19,70 @@ def patients_index(request):
     return render(request, 'patients/index.html')
 def registration_signup(request):
     return render(request, 'registration/signup.html')
+
+
+#   Patients index, and detail views
+def patients_index(request):
+#   patients = Patient.objects.filter(user=request.user)
+  return render(request, 'patients/index.html', { 'patients': patients })
+
+def patients_detail(request, patient_id):
+#   patient = Patient.objects.get(id=patient_id)
+  return render(request, 'patient/detail.html')
+
+# Pills create, update, and delete views
+class PillsCreate(CreateView):
+  model = Pills
+  fields = '__all__'
+  success_url = '/patients/'
+
+class PillsUpdate(UpdateView):
+    model = Pills
+    fields = '__all__'
+
+class PillsDelete(DeleteView):
+    model = Pills
+    success_url = '/patients/'
+
+# Appointments create, update, and delete views
+class AppointmentsCreate(CreateView):
+  model = Appointments
+  fields = '__all__'
+  success_url = '/patients/'
+
+class AppointmentsUpdate(UpdateView):
+    model = Appointments
+    fields = '__all__'
+
+class AppointmentsDelete(DeleteView):
+    model = Appointments
+    success_url = '/patients/'
+
+# Patient create, update, and delete views
+class PatientsCreate(CreateView):
+  model = Patient
+  fields = '__all__'
+  success_url = '/patients/'
+
+class PatientsUpdate(UpdateView):
+    model = Patient
+    fields = '__all__'
+
+class PatientsDelete(DeleteView):
+    model = Patient
+    success_url = '/patients/'
+
+# signup views
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
